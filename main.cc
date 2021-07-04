cc8ac0dc83e68e9a7726adb1281ca8e31ceb3ade
tan86 <jayparmar7072@gmail.com>
2021-07-04 19:05:48 +0530

    Added File read feature.
    Now you can pass BF source code as argument.

--- main.cc
+++ main.cc
@@ -1,59 +1,69 @@
+#include <cstdio>
 #include <iostream>
 #include <string>
+#include <string_view>
 
-int main() {
+void interpret(std::string_view src) {
     char mem[256]{};
     std::uint8_t p = 0;
-    std::string inputStr{};
-
-    std::cout << "Brain Fuck Interpreter v1.0\n";
-    for (;;) {
-        std::cout << "> ";
-        std::getline(std::cin, inputStr);
-
-        if (inputStr == "exit") {
-            break;
-        }
-
-        for (std::uint8_t i = 0; i < inputStr.size(); ++i) {
-            switch (inputStr[i]) {
-                case '>': ++p; break;
-                case '<': --p; break;
-                case '+': ++mem[p]; break;
-                case '-': --mem[p]; break;
-                case '.': std::cout << mem[p]; break;
-                case ',': std::cin >> mem[p]; break;
-                case '[':
-                    if (mem[p] == 0) {
-                        int open = 0;
-                        while (inputStr[++i]) {
-                            if (inputStr[i] == ']' && open == 0) {
-                                break;
-                            } else if (inputStr[i] == '[') {
-                                ++open;
-                            } else if (inputStr[i] == ']') {
-                                --open;
-                            }
-                        }
+    for (std::uint8_t i = 0; src[i] != 0; ++i) {
+        switch (src[i]) {
+            case '>': ++p; break;
+            case '<': --p; break;
+            case '+': ++mem[p]; break;
+            case '-': --mem[p]; break;
+            case '.': std::cout << mem[p]; break;
+            case ',': std::cin >> mem[p]; break;
+            case '[':
+                if (mem[p] == 0) {
+                    std::uint8_t open = 0;
+                    while (src[++i]) {
+                        if (src[i] == ']' && open == 0) break;
+                        else if (src[i] == '[') ++open;
+                        else if (src[i] == ']') --open;
                     }
-                    break;
-                case ']':
-                    if (mem[p] != 0) {
-                        int close = 0;
-                        while (inputStr[--i]) {
-                            if (inputStr[i] == '[' && close == 0) {
-                                break;
-                            } else if (inputStr[i] == ']') {
-                                ++close;
-                            } else if (inputStr[i] == '[') {
-                                --close;
-                            }
-                        }
+                }
+                break;
+            case ']':
+                if (mem[p] != 0) {
+                    std::uint8_t close = 0;
+                    while (src[--i] != 0) {
+                        if (src[i] == '[' && close == 0) break;
+                        else if (src[i] == ']') ++close;
+                        else if (src[i] == '[') --close;
                     }
-                    break;
-                default: break;
-            }
+                }
+                break;
+            default: break;
         }
     }
+}
+
+int main(int argc, char* argv[]) {
+    std::string inputStr{};
+
+    if (argc != 2) {
+        std::cout << "Brain Fuck Interpreter v1.0\n";
+        for (;;) {
+            std::cout << "> ";
+            std::getline(std::cin, inputStr);
+
+            if (inputStr == "exit") break;
+            interpret(inputStr);
+        }
+    } else {
+        FILE* inputFile = nullptr;
+        inputFile = fopen(argv[1], "r");
+
+        if (inputFile) {
+            char ch{};
+            while ((ch = getc(inputFile)) != EOF) { inputStr += ch; }
+
+            interpret(inputStr);
+
+            fclose(inputFile);
+        }
+    }
+
     return 0;
 }
