cc95c7be5e7087b07decd2173c6eadb9e48cae8b
tan86 <jayparmar7072@gmail.com>
2021-07-07 09:19:54 +0530

    Added smaller version of interpreter.
    that just reads from file.

---
+++ small.cc
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <cstdint>
+
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        if(FILE* inputFile = fopen(argv[1], "r")){
+            char src[512]{},ch{};
+            for(uint8_t i{}; (ch = getc(inputFile)) != EOF; ++i){ src[i] = ch; }
+            fclose(inputFile);
+            for (uint8_t i{},p{},mem[256]{}; src[i] != 0; ++i) {
+                if(src[i] == '>'){ ++p; }
+                else if(src[i] == '<') { --p; }
+                else if(src[i] == '+') { ++mem[p]; }
+                else if(src[i] == '-') { --mem[p]; }
+                else if(src[i] == '.') { putchar(mem[p]); }
+                else if(src[i] == ',') { mem[p] = getchar(); }
+                else if(src[i] == '[' && mem[p] == 0){
+                    for(uint8_t b{};src[++i];) {
+                        if(src[i] == '[') ++b;
+                        else if(src[i] == ']' && b-- == 0) break;
+                    }
+                }
+                else if(src[i] == ']' && mem[p] != 0){
+                    for(uint8_t b{};src[--i];) {
+                        if(src[i] == ']') ++b;
+                        else if(src[i] == '[' && b-- == 0) break;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
